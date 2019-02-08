952511df5852aeb8656b0e685a36b96cf69c8935
James-Ackerman <james.ackerman@upr.edu>
2019-02-08 17:41:13 -0400

    Initial Code

---
+++ src/autonomous.cpp
@@ -0,0 +1,30 @@
+#include "main.h"
+//#include "okapi/api.hpp"
+//#include "definitions.hpp"
+//using namespace okapi;
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {
+
+  //IntakeR.move_voltage(12000);
+  //pros::delay(500);
+  // state != state;
+  // sensor.set_value(state);
+  //
+  // rotatorController.setTarget(180_deg);     //flip intake
+  // conveyorController.setTarget(200_rpm);    //move conveyor full speed
+  // liftController.setTarget(50_deg);         //raise lift //TODO: measure distance per degree
+  // driveController.moveDistanceAsync(1_m);   // Move 1 meter to the first goal
+  // driveController.turnAngle(90_deg);        // Turn 90 degrees
+  //pros::delay(1000) or pros::Task::delay(1000) idk which one
+}
---
+++ src/opcontrol.cpp
@@ -0,0 +1,109 @@
+#include "main.h"
+//#include "okapi/api.hpp"
+#include "definitions.hpp"
+//using namespace okapi; //
+
+//TODO: check syntax for ControllerButton for R1/R2/L1/L2/Down/left/right buttons
+ControllerButton RightBumperUP(ControllerDigital::R1);
+ControllerButton RightBumperDOWN(ControllerDigital::R2);
+ControllerButton LeftBumperUP(ControllerDigital::L1);
+ControllerButton LeftBumperDOWN(ControllerDigital::L2);
+ControllerButton ButtonA(ControllerDigital::A);
+ControllerButton ButtonB(ControllerDigital::B);
+ControllerButton ButtonX(ControllerDigital::X);
+ControllerButton ButtonY(ControllerDigital::Y);
+ControllerButton ButtonUP(ControllerDigital::up);
+ControllerButton ButtonDOWN(ControllerDigital::down);
+ControllerButton ButtonLEFT(ControllerDigital::left);
+ControllerButton ButtonRIGHT(ControllerDigital::right);
+////////////////////////////////////OPCONTROL///////////////////////////////////////////////////
+
+void opcontrol() {
+
+  Controller controller;
+  bool STATE = LOW;                        //For Pneumatics //Change to false if this doesn't not work
+  int FLYWHEEL_STATE = 1;
+  int FLYWHEEL_TARGET = 0;
+  int FLYWHEEL_LOW = 400;
+  int FLYWHEEL_MID = 500;
+  int FLYWHEEL_HIGH = 600;
+  linetrackerL.calibrate();
+  linetrackerR.calibrate();
+  gyro.calibrate();
+
+  pros::Task FwControl(FwControlTask);
+
+  //auto flyController = AsyncControllerFactory::velPID(FLYWHEEL_MOTOR, 1.0, 0.01, 0.1);
+  flywheel.setGearing(AbstractMotor::gearset::blue);
+  while (true)
+	{
+     // pros::lcd::initialize();
+     // pros::lcd::print(0, "Joystick valY: %d", controller.getAnalog(ControllerAnalog::leftY));
+     // pros::lcd::print(0, "Joystick valX: %d", controller.getAnalog(ControllerAnalog::leftX));
+		 //////////////////////////////CHASSIS(DRIVE)///////////////////////////////
+
+     driveController.arcade(controller.getAnalog(ControllerAnalog::leftY), -controller.getAnalog(ControllerAnalog::rightX));
+
+     //////////////////////////////TRANSMISSION/////////////////////////////////
+     if (ButtonB.changedToPressed())
+     {
+         STATE = !STATE;
+         piston.set_value(STATE);
+     }
+     /////////////////////////////////INTAKE////////////////////////////////////
+     if (RightBumperDOWN.isPressed())          //Hold button to Chupa Intake
+     {
+         intake.move_voltage(12000);
+         intake.move_voltage(12000);
+     }
+     else if (RightBumperUP.isPressed())  //Hold button to Bota Intake
+     {
+         intake.move_voltage(-12000);
+         intake.move_voltage(-12000);
+     }
+     else                                   //Intake stops if nothing is pressed
+     {
+         intake.move_voltage(0);
+         intake.move_voltage(0);
+     }
+     ///////////////////////////////INDEXER////////////////////////////////////
+     if (LeftBumperDOWN.isPressed())
+     {
+         indexer.move_voltage(12000);
+     }
+     else if (LeftBumperUP.isPressed())
+     {
+         indexer.move_voltage(-12000);
+     }
+     else
+     {
+         indexer.move_voltage(0);
+     }
+     /////////////////////////////FLYWHEEL////////////////////////////////
+      if (ButtonDOWN.changedToPressed())             //Press button to flip intake
+      {
+              FwVelocitySet(400, 1);
+      }
+
+      else if (ButtonRIGHT.changedToPressed())         //Flywheel MID conditions
+      {
+              FwVelocitySet(500, 1);
+      }
+
+      else if (ButtonUP.changedToPressed())             //Flywheel HIGH conditions
+      {
+              FwVelocitySet(600, 1);
+      }
+
+      else if (ButtonLEFT.changedToPressed())
+      {
+              FwVelocitySet(0, 1);
+      }
+
+		 /////////////////////////////////DESCORER//////////////////////////////////////
+
+				 descorer.move_voltage(12000*controller.getAnalog(ControllerAnalog::rightY));
+
+		 pros::Task::delay(20);
+	 }
+}
